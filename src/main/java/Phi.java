8e55de8c3190cd325c471ed9398c0552a17f2fae
sciphi-123 <ryuni2123@gmail.com>
2025-01-24 15:40:41 +0800

    ToDo, Event, Deadline

--- src/main/java/Phi.java
+++ src/main/java/Phi.java
@@ -37,31 +37,51 @@ public class Phi {
             int doneIndex = Integer.parseInt(userInput.substring(5));
             Task doneTask = this.list.get(doneIndex - 1);
             doneTask.markDone();
-            System.out.println(String.format("%s %s", doneTask.getStatusIcon(), doneTask.getDescription()));
+            System.out.println(doneTask);
             System.out.println();
         } else if (userInput.startsWith("unmark ")) {
             // "unmark " contains 7 characters.
             int undoneIndex = Integer.parseInt(userInput.substring(7));
             Task undoneTask = this.list.get(undoneIndex - 1);
             undoneTask.markUndone();
-            System.out.println(String.format("%s %s", undoneTask.getStatusIcon(), undoneTask.getDescription()));
+            System.out.println(undoneTask);
             System.out.println();
-        } else {
-            this.addInput(new Task(userInput));
-            this.echo(userInput);
+        } else if (userInput.startsWith("todo ")){
+            String description = userInput.substring(5);
+            this.addInput(new Todo(description));
+        } else if (userInput.startsWith("deadline ")) {
+            // remove task type; "deadline " contains 9 characters
+            String[] clean = userInput.substring(9).split(" /");
+            this.addInput(new Deadline(clean[0], clean[1].substring(3)));
+        } else if (userInput.startsWith("event ")) {
+            String[] clean = userInput.substring(6).split(" /");
+            this.addInput(new Event(clean[0], clean[1].substring(5), clean[2].substring(3)));
         }
         this.checkInput();
     }
 
     public void addInput(Task userTask) {
+        System.out.println("Got it. I've added this task:");
+        System.out.println(String.format("  %s", userTask));
         this.list.add(userTask);
+        this.taskCount();
+    }
+
+    public void taskCount() {
+        int listSize = this.list.size();
+        if (listSize == 1) {
+            System.out.println("Now you have 1 task in the list.");
+        } else {
+            System.out.println(String.format("Now you have %d tasks in the list.", listSize));
+        }
+        System.out.println();
     }
 
     public void listInput() {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < this.list.size(); i++) {
             Task temp = this.list.get(i);
-            System.out.println(String.format("%d.%s %s", i + 1, temp.getStatusIcon(), temp.getDescription()));
+            System.out.println(String.format("%d.%s", i + 1, temp));
         }
         System.out.println();
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-public class Task {
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
@@ -23,6 +23,11 @@ public class Task {
     public void markUndone() {
         System.out.println("Ok, I've marked this task as not done yet:");
         this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.getStatusIcon(), this.getDescription());
     }
 }
 
