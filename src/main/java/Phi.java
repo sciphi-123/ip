d9067293d692fe600b7bb191d7f3f1d71ff21b2b
sciphi-123 <ryuni2123@gmail.com>
2025-01-24 11:40:35 +0800

    Add, List

--- src/main/java/Phi.java
+++ src/main/java/Phi.java
@@ -1,32 +1,53 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Phi {
-    private void greet() {
+    private ArrayList<String> list;
+
+    public Phi() {
+        this.list = new ArrayList<>();
+    }
+
+    public void greet() {
         System.out.println("Hello! I'm Phi!");
         System.out.println("What can I do for you?");
         System.out.println();
     }
 
-    private void echo(String input) {
-        System.out.print("Phi: ");
+    public void echo(String input) {
+        System.out.print("added: ");
         System.out.println(input);
         System.out.println();
     }
 
-    private void exit() {
-        System.out.println("Phi: Bye. Hope to see you again soon!");
+    public void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
-    private void checkInput() {
+    public void checkInput() {
         Scanner input = new Scanner(System.in);
-        System.out.print("User: ");
         String userInput = input.next();
         if (userInput.equals("bye")) {
             this.exit();
             return;
+        } else if (userInput.equals("list")) {
+            this.listInput();
+        } else {
+            this.addInput(userInput);
+            this.echo(userInput);
         }
-        this.echo(userInput);
         this.checkInput();
+    }
+
+    public void addInput(String userInput) {
+        this.list.add(userInput);
+    }
+
+    public void listInput() {
+        for (int i = 0; i < this.list.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, this.list.get(i)));
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
